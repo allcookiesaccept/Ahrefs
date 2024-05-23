9cb14542958542bf9c724b3c596c8bb6a59a1668
allcookiesaccept <insufferable.lobster@gmail.com>
2024-05-23 13:00:18 +0300

    add Ahrefs class

--- ahrefs.py
+++ ahrefs.py
@@ -6,12 +6,17 @@ from settings import DataManager
 dm = DataManager.get_instance()
 TOKEN = dm.token
 
+class Ahrefs:
+
+    def __init__(self, token=TOKEN):
+        self.methods = AhrefsMethods(token)
+        self.tasks = AhrefsTasks(self.methods)
+
 
 if __name__ == "__main__":
-    methods = AhrefsMethods(TOKEN)
-    tasks = AhrefsTasks(methods)
 
+    ahrefs = Ahrefs()
     file_name = "domain_ratings_dynamics_from_2024.xlsx"
-    file_path = os.path.join(tasks.output_files_folder, file_name)
-    df = tasks.compare_data_rating()
+    file_path = os.path.join(ahrefs.tasks.output_files_folder, file_name)
+    df = ahrefs.tasks.compare_data_rating()
     df.to_excel(file_path, index=False)
